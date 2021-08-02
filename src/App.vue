22d11ddf91bf986f6f56b855766db2b82e9859a8
ViavaCos <13811092694@163.com>
2021-08-02 08:47:25 +0800

    feat: 新增导航

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/virtual-list">virtual-list</router-link> |
-      <router-link to="/traversal">traversal</router-link>
+      <router-link v-for="route in routes" :key="route.name" :to="route.path">
+        {{ route.name }}
+      </router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
+<script>
+import router from './router'
+export default {
+  name: 'APP',
+  data(){
+    return {
+      routes: router.options.routes || []
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,12 +31,24 @@
 }
 
 #nav {
+  display: flex;
+  flex-wrap: nowrap;
   padding: 2px;
+  height: 20px;
+  word-break: keep-all;
+  overflow-x: auto;
+  overflow-y: hidden;
 }
 
 #nav a {
+  flex: 1;
+  width: fit-content;
+  padding: 0 2px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #333333;
+  &:nth-child(n+2){
+    border-left: 1px solid #999;
+  }
 }
 
 #nav a.router-link-exact-active {
